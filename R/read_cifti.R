3f03e2967b8a14a3527c1336c2cb3664a4471841
John Muschelli <muschellij2@gmail.com>
2018-02-01 22:25:24 +0000

    version 0.4.5

--- R/read_cifti.R
+++ R/read_cifti.R
@@ -7,8 +7,9 @@
 #' @return List of information from the CIFTI file
 #' @export
 #' @examples
-#' if (have_cifti_test_data()) {
-#'    files = download_cifti_data()
+#' outdir = tempdir()
+#' if (have_cifti_test_data(outdir = outdir)) {
+#'    files = download_cifti_data(outdir = outdir)
 #'    fname = grep("MyelinAndCorrThickness.32k_fs_LR.dscalar",
 #'    files, value = TRUE)
 #'    res = read_cifti(fname)
@@ -16,25 +17,29 @@
 read_cifti = function(fname,
                       drop_data = TRUE,
                       trans_data = TRUE) {
+  fname = nii_fname(fname)
   res = get_cifti_type(fname)
   data = cifti_data(fname)
 
   #########################################
   # Dropping empty dimensions
   #########################################
+  orig_dim = dim(data)
   if (drop_data) {
     data = drop(data)
   }
   attr(data, "drop") = drop_data
+  attr(data, "orig_dim") = orig_dim
 
   #########################################
   # Transposing data
   #########################################
   ddata = dim(data)
-  if (length(ddata) == 2) {
-    if (trans_data) {
+  if (trans_data) {
+    if (length(ddata) == 2) {
       data = t(data)
     } else {
+      trans_data = FALSE
       warning("Dimensions of the data > 2, so no transposing done!")
     }
   }
@@ -43,6 +48,10 @@ read_cifti = function(fname,
   res$data = data
   hdr = nifti_2_hdr(fname)
   res$hdr = hdr
+  res$filename = fname
+  res$drop_data = drop_data
+  res$trans_data = trans_data
+
   class(res) = "cifti"
 
   return(res)
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # cifti
 
-[![Travis-CI Build Status](https://travis-ci.org/muschellij2/cifti.svg?branch=master)](https://travis-ci.org/muschellij2/cifti)# cifti
+This is a Toolbox for Connectivity Informatics Technology Initiative ('CIFTI') Files
+    
+[![Travis-CI Build Status](https://travis-ci.org/muschellij2/cifti.svg?branch=master)](https://travis-ci.org/muschellij2/cifti)
+
+[![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/muschellij2/cifti?branch=master&svg=true)](https://ci.appveyor.com/project/muschellij2/cifti)
+
+[![Coverage Status](https://img.shields.io/coveralls/muschellij2/cifti.svg)](https://coveralls.io/r/muschellij2/cifti?branch=master)
+
+## Installation
+
+You can install cifti from github with:
+
+
+``` r
+# install.packages("devtools")
+devtools::install_github("muschellij2/cifti")
+```
